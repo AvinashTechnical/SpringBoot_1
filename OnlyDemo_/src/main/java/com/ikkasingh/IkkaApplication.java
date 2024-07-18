8f31ab0fc8490b236509b6c08f38d74319ddb38d
AvinashTechnical <technical.avinash.gupta001@gmail.com>
2024-07-18 20:40:58 +0530

    Add files via upload

---
+++ OnlyDemo_/src/main/java/com/ikkasingh/IkkaApplication.java
@@ -0,0 +1,14 @@
+package com.ikkasingh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkkaApplication.class, args);
+		System.out.println("Hello spring");
+	}
+
+}
